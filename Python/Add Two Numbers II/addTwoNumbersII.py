e28934e077e71ca72d7e9871ea8a9c4f460eaecd
bartlomiej-niemiec <bniemiec11@gmail.com>
2023-10-22 23:00:31 +0200

    solution

--- Python/Add Two Numbers II/addTwoNumbersII.py
+++ Python/Add Two Numbers II/addTwoNumbersII.py
@@ -25,8 +25,41 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        l1_stack = []
+        l2_stack = []
 
+        current_node = l1
+        while current_node != None:
+            l1_stack.append(current_node)
+            current_node = current_node.next
 
+        current_node = l2
+        while current_node != None:
+            l2_stack.append(current_node)
+            current_node = current_node.next
 
+        current_node = None
+        prev_rest = 0
+        while (l1_stack or l2_stack or prev_rest != 0):
+            if l1_stack:
+                l1_node = l1_stack.pop()
+            else:
+                l1_node = ListNode(val=0)
+            if l2_stack:
+                l2_node = l2_stack.pop()
+            else:
+                l2_node = ListNode(val=0)
+            sum_rest = (l1_node.val + l2_node.val + prev_rest) // 10
+            if sum_rest:
+                sum_val = (l1_node.val + l2_node.val + prev_rest) - sum_rest * 10
+                prev_rest = sum_rest
+            else:
+                sum_val = l1_node.val + l2_node.val + prev_rest
+                prev_rest = 0
+            new_node = ListNode(val=sum_val)
+            new_node.next = current_node
+            current_node = new_node
+
+        return current_node
 
 
